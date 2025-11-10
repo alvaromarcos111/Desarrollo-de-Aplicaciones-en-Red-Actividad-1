5d3b4a387b28830dabf0f24c6aa49c0c9e180b46
Alvaro <alvaromarcos111@gmail.com>
2025-11-10 21:47:26 +0100

    error, no se ven imagenes en github page repeat anterior commit

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 window.addEventListener("scroll", function(){
-    const button = document.querySelector("#volver");
+    const boton = document.querySelector("#volver");
     // Mostrar el botón solo si el usuario ha llegado al final de la página
     if((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight) {
-        button.style.display = "flex";
+        boton.style.display = "flex";
     }else {
-        button.style.display = "none";
+        boton.style.display = "none";
     }
 });
 
 // Evento click para que al hacer clic el botón haga scroll hacia arriba
-button.addEventListener("click", function(){
+botonvolver = document.querySelector(".button");
+botonvolver.addEventListener("click", function(){
     window.scrollTo({top: 0, behavior: "smooth"});
 });
 
